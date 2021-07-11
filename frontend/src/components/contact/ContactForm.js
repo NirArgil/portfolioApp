2446898b3efee337c614be4cb218e80e4e66360e
NIR <MAILMAIL10@GMAIL.COM>
2021-07-11 15:27:01 +0300

    Opt

--- frontend/src/components/contact/ContactForm.js
+++ frontend/src/components/contact/ContactForm.js
@@ -37,7 +37,7 @@ const ContactForm = () => {
 
        //http://localhost:3001/send on dev
         // eslint-disable-next-line no-unused-vars
-        const response = await fetch("/send", {  
+        const response = await fetch("/api/send", {  
             method: "POST",
             headers: {
                 "Content-type": "application/json",
