0d2aa03ad802230fea146abc7e87bb54761210cc
NIR <MAILMAIL10@GMAIL.COM>
2021-07-11 13:02:56 +0300

    Opt

--- frontend/src/components/contact/ContactForm.js
+++ frontend/src/components/contact/ContactForm.js
@@ -41,7 +41,7 @@ const ContactForm = () => {
             method: "POST",
             headers: {
                 "Content-type": "application/json",
-                "Accept": "application/json",
+                // "Accept": "application/json",
             },
             body: JSON.stringify({ mailerState }),
         })
