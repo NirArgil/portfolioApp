f2f4e89140bcc9a23169265692193553259a4936
NIR <MAILMAIL10@GMAIL.COM>
2021-07-13 20:04:16 +0300

    Opt

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.scss';
 export default function Footer() {
   return (
     <footer>
-      <div> Nir Argil 2021</div>
+      <div> All Rights Reserved © Nir Argil 2021</div>
       <div>icons by <a href="https://icons8.com/">icons8</a></div>
     </footer>
   )
